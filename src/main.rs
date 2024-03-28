a2579ffe2f81b830966e6d204db7d13d5495d1cc
DeepakNPalyam <deepaknpalyam120@gmail.com>
2024-03-28 11:44:55 +0530

    user input and guess checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use rand::Rng;
+
+pub fn generate_secret_number() -> u32 {
+    rand::thread_rng().gen_range(1..=100)
+}
+
+use std::io;
+
+pub fn prompt_guess() -> u32 {
+    println!("Please enter your guess (between 1 and 100):");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    guess.trim().parse().expect("Please enter a valid number!")
+}
 
 fn main() {
     println!("Welcome to the Guessing Game!");
